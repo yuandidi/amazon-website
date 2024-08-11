7dc95ca1dddefdcc1229abe672739a976d69617d
yuandidi <2330919181@qq.com>
2024-08-11 20:03:28 +0800

    add the esercise 14

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -61,15 +61,11 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
+export function updateCartQuantity() {
+  let cartQuantity = calculateCartQuantity();
 
-      cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart')
@@ -86,3 +82,5 @@ document.querySelectorAll('.js-add-to-cart')
       addedElement.classList.add("showed-added-message");
     });
   });
+
+  updateCartQuantity();
