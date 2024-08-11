e3f3fa67b9b5a92e5dafa5868b5bfd07f124fdc8
yuandidi <2330919181@qq.com>
2024-08-11 15:07:59 +0800

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+      const valueSelected = document.querySelector(`.js-quantity-selector-${productId}`);
+
+      cart.forEach((cartItem) => {
+        if(productId === cartItem.productId) {
+          matchingItem = cartItem;
+        }
+      });
+
+      if(matchingItem) {
+        matchingItem.quantity += Number(valueSelected.value);
+      } else {
+        cart.push({
+          productId,
+          quantity: Number(valueSelected.value)
+        });
+      }
+}
