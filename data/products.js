e830c5cdf3c23c7ce10925750217f23dec62bcb2
yuandidi <2330919181@qq.com>
2024-08-14 13:48:07 +0800

    Convert from promises to async await

--- data/products.js
+++ data/products.js
@@ -98,6 +98,31 @@ const object2 = {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json()
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'food') {
+        return new Food(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -112,7 +137,7 @@ export function loadProducts(fun) {
     });
 
     console.log('load products');
-    
+
     fun();
   });
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,28 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() =>  {
-      resolve('value1');
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
     });
-  }),
+  })
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+/*
+Promise.all([
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
@@ -24,6 +35,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 /*
 new Promise((resolve) => {
   loadProducts(() =>  {
