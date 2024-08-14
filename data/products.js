6b8491146bea113d3f78ae072a74046e5285bcc0
yuandidi <2330919181@qq.com>
2024-08-14 12:35:30 +0800

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -96,7 +96,32 @@ const object2 = {
 };
 */
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'food') {
+        return new Food(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+    
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -797,3 +822,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,12 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
