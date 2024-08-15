a9d7ec8cee2adde0eb6bf6d558845a46fff856ed
元迪迪 <143935444+yuandidi@users.noreply.github.com>
2024-08-15 16:06:37 +0800

    Update amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -118,14 +118,14 @@ function renderProductsGrid() {
     document.querySelector('.js-search-button')
     .addEventListener('click', () => {
       const search = document.querySelector('.js-search-bar').value;
-      window.location.href = `amazon.html?search=${search}`;
+      window.location.href = `index.html?search=${search}`;
     });
 
     document.querySelector('.js-search-bar')
     .addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         const searchTerm = document.querySelector('.js-search-bar').value;
-        window.location.href = `amazon.html?search=${searchTerm}`;
+        window.location.href = `index.html?search=${searchTerm}`;
       }
     });
 }
